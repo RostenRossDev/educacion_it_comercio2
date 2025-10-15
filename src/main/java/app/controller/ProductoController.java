407336c42601a3cda18520679a636eee90350802
Costantini nestor matias <costantini.nestor.m@gmail.com>
2025-10-15 20:34:54 -0300

    first commit

---
+++ src/main/java/app/controller/ProductoController.java
@@ -0,0 +1,9 @@
+package app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class ProductoController {
+
+}
